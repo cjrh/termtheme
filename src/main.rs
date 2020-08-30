7c978d842736d4015c6a24cedc3995e2e8ce7396
Caleb Hattingh <caleb.hattingh@gmail.com>
2020-08-30 23:10:45 +1000

    Loading and saving json is working

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,25 @@ use std::path::Path;
 
 use json;
 
+const KEYS: &'static [&'static str] = &[
+    "terminal.ansiBlack",
+    "terminal.ansiBlue",
+    "terminal.ansiCyan",
+    "terminal.ansiGreen",
+    "terminal.ansiMagenta",
+    "terminal.ansiRed",
+    "terminal.ansiWhite",
+    "terminal.ansiYellow",
+    "terminal.ansiBrightBlack",
+    "terminal.ansiBrightBlue",
+    "terminal.ansiBrightCyan",
+    "terminal.ansiBrightGreen",
+    "terminal.ansiBrightMagenta",
+    "terminal.ansiBrightRed",
+    "terminal.ansiBrightWhite",
+    "terminal.ansiBrightYellow",
+];
+
 #[derive(structopt::StructOpt)]
 struct Args {
     #[structopt(short, long)]
@@ -35,7 +54,11 @@ fn main(args: Args) {
         &path, // "/org/gnome/terminal/legacy/profiles:/:5db53530-b692-4860-95df-0fef50b1d52c/",
     );
     println!("{:?}", &terminal_profile);
+    let foreground_color = &terminal_profile.get_string("foreground-color").unwrap();
+    let background_color = &terminal_profile.get_string("background-color").unwrap();
     let palette = &terminal_profile.get_strv("palette");
+    println!("foreground-color {:?}", &foreground_color);
+    println!("background-color {:?}", &background_color);
     println!("palette {:?}", &palette);
 
     for c in palette {
@@ -65,6 +88,12 @@ fn main(args: Args) {
     println!("Loading the file again:");
     let loaded = load_theme("beep.gnome-terminal-theme");
     println!("{:?}", &loaded);
+
+    println!("{}", "dumping to vscode");
+    dump_theme_vscode(&vs, "beep.gnome-terminal-theme.json");
+    println!("{}", "loading from vscode");
+    let loaded = load_theme_vscode("beep.gnome-terminal-theme.json");
+    println!("{:?}", &loaded);
 }
 
 fn dump_theme(theme: &Vec<String>, filename: &str) {
@@ -80,4 +109,38 @@ fn load_theme(filename: &str) -> Vec<String> {
     let mut file = File::open(&path).unwrap();
     file.read_to_string(&mut data).unwrap();
     data.split("\t").map(str::to_string).collect()
+}
+
+fn dump_theme_vscode(theme: &Vec<String>, filename: &str) {
+    let path = Path::new(filename);
+    let mut file = File::create(&path).unwrap();
+    let mut data = json::JsonValue::new_object();
+    data["workbench.colorCustomizations"] = json::JsonValue::new_object();
+    data["workbench.colorCustomizations"]["terminal.foreground"] =
+        json::JsonValue::String(theme[0].clone());
+    data["workbench.colorCustomizations"]["terminal.background"] =
+        json::JsonValue::String(theme[0].clone());
+
+    KEYS.iter().enumerate().for_each(|(i, &key)| {
+        let rgb = theme[i].clone();
+        let hex = rgb.parse::<Rgb>().unwrap().to_css_hex_string();
+        data["workbench.colorCustomizations"][key] = json::JsonValue::String(hex);
+    });
+    file.write_all(data.pretty(4).as_bytes()).unwrap();
+}
+
+fn load_theme_vscode(filename: &str) -> Vec<String> {
+    let path = Path::new(filename);
+    let mut data = String::new();
+    let mut file = File::open(&path).unwrap();
+    file.read_to_string(&mut data).unwrap();
+
+    let parsed = json::parse(&data).unwrap();
+    // println!("Loaded json: {}", parsed);
+    KEYS.iter()
+        .map(|&color| {
+            let hex = parsed["workbench.colorCustomizations"][color].to_string();
+            Rgb::from_hex_str(&hex).unwrap().to_css_string()
+        })
+        .collect::<Vec<String>>()
 }
